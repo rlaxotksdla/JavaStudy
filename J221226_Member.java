2b3b22a69a111c1656405300d7afcceba9b5b7a7
rlaxotksdla <120385890+rlaxotksdla@users.noreply.github.com>
2022-12-26 17:49:15 +0900

    221226

---
+++ J221226_Member.java
@@ -0,0 +1,14 @@
+
+public class J221226_Member implements J221226_Creature {
+	
+    @Override
+	public void method(){
+		System.out.println("인터페이스 Creature를 구현한 Member의 method() 호출됨.");
+	}
+	
+    public void method1(){
+    	System.out.println("Member 클래스에 있는 Member Method1() 호출됨.");
+    }
+    
+	
+}
