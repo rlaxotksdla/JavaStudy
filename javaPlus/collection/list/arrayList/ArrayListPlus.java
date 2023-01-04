f14464b1af6d8250f07f077e3a320b5a24d1b02d
rlaxotksdla <120385890+rlaxotksdla@users.noreply.github.com>
2023-01-04 12:34:54 +0900

    230104

---
+++ javaPlus/collection/list/arrayList/ArrayListPlus.java
@@ -0,0 +1,19 @@
+/*
+ * Class ArrayListPlus
+ * Date 23.01.04
+ */
+
+package javaPlus.collection.list.arrayList;
+
+import java.util.ArrayList;
+
+public class ArrayListPlus{
+	
+	public static void main(String[] args) {
+//		ArrayList arrayList = new ArrayList();
+		ArrayList<> arrayList = new ArrayList<>();
+		arrayList.add("A");
+	
+	}
+	
+}
