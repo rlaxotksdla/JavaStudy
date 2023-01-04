ada1010003c655a346ef63d9cba2b15b6f09721f
rlaxotksdla <120385890+rlaxotksdla@users.noreply.github.com>
2023-01-04 17:16:15 +0900

    230104

--- javaPlus/collection/list/arrayList/ArrayListCompletion.java
+++ javaPlus/collection/list/arrayList/ArrayListCompletion.java
@@ -10,17 +10,30 @@ import java.util.*;
 
 public class ArrayListCompletion {
     public static void main(String[] args) {
+        StoreManageMent.
 
     }
+}
+
+class StoreManageMent{
+    static ArrayList<Store> stores = new ArrayList<Store>();
+
+    static void addStore(String manager, String location){
+        stores.add(new Store(manager, location));
+    }
+
 }
 
 class Store{
     final String manager;
     final String location;
+    ArrayList<String> members = new ArrayList<String>();
 
     Store(String manager, String location){
         this.manager = manager;
         this.location = location;
         System.out.println(this.location + "지점이 등록되었습니다. (지점장 : " + this.manager);
     }
+
+
 }
